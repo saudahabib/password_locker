424fbefd7c9e1c9b809971bd8d9e6f1167d224ec
saudahabib <saudababs00@gmail.com>
2019-04-05 22:38:20 +0300

    write code for adding, deleting, viewing, verifying a User

--- passwords.py
+++ passwords.py
@@ -1,3 +1,4 @@
+import pyperclip
 class User :
     '''
     Class that generates new instances of a User
@@ -16,3 +17,44 @@ class User :
         '''
         delete_contact method deletes the user info from contact_list
         '''
+        User.user_list.remove(self)
+    @classmethod
+    def find_by_account(cls,account):
+        '''
+        Method takes in account name and displays user info for that particular account
+        Args:
+            Account name to search for
+        Returns:
+            User info for that account
+        '''
+        for details in cls.user_list:
+            if details.account == account:
+                return details
+
+    @classmethod
+    def user_exists(cls,account):
+        '''
+        Method checks if user exists from the user_list
+        Args:
+            account : Account to search if user exists from user_list
+        Return:
+            Boolean: True or false depending if the user exists
+        '''
+
+        for usern in cls.user_list:
+            if contact.account == account:
+                return True
+
+        return False
+
+    @classmethod
+    def display_users(cls):
+        '''
+        Method that displays all users
+        '''
+        return cls.user_list
+
+    @classmethod
+    def copy_password(cls, account):
+        user_found = User.find_by_account(account)
+        pyperclip.copy(user_found.username)
